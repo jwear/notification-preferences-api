b4da80b1b4aeb0b6816bf3bd03d84e679be4408b
Julie Anne Wear <ja.wear@pm.me>
2026-09-09 16:55:10 -0700

    Add post endpoint

--- src/app.js
+++ src/app.js
@@ -3,10 +3,41 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+const preferences = [];
+let nextId = 0;
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Running");
+app.post("/preferences", (req, res) => {
+  const { userId, type, email, sms, push } = req.body;
+
+  if (
+    !userId ||
+    !type ||
+    email === undefined ||
+    sms === undefined ||
+    push === undefined
+  ) {
+    return res
+      .status(400)
+      .json({ error: "userId, type, email, sms, and pus are required" });
+  }
+
+  if (
+    typeof userId !== "number" ||
+    typeof type !== "string" ||
+    typeof email !== "boolean" ||
+    typeof sms !== "boolean" ||
+    typeof push !== "boolean"
+  ) {
+    return res.status(400).json({ error: "Invalid field types" });
+  }
+
+  const preference = { id: nextId, userId, type, email, sms, push };
+  preferences.push(preference);
+  nextId++;
+
+  res.status(201).json(preference);
 });
 
 app.listen(PORT, () => {
