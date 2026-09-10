212ed7c90d4b8918fb6b00bdbde6a792ab52abdc
Julie Anne Wear <ja.wear@pm.me>
2026-09-10 14:49:40 -0700

    Add patch endpoint

--- src/app.js
+++ src/app.js
@@ -56,6 +56,51 @@ app.get("/preferences/:id", (req, res) => {
   res.json(preference);
 });
 
+app.patch("/preferences/:id", (req, res) => {
+  const id = req.params.id;
+  const { userId, type, email, sms, push } = req.body;
+
+  const preference = preferences.find((p) => p.id === Number(id));
+
+  if (!preference) {
+    return res.status(404).json({ error: "Notification preference not found" });
+  }
+
+  if (
+    (userId && typeof userId !== "number") ||
+    (type && typeof type !== "string") ||
+    (email && typeof email !== "boolean") ||
+    (sms && typeof sms !== "boolean") ||
+    (push && typeof push !== "boolean")
+  ) {
+    return res.status(400).json({ error: "Invalid field types" });
+  }
+
+  console.log(req.body);
+
+  if (userId) {
+    preference.userId = userId;
+  }
+
+  if (type) {
+    preference.type = type;
+  }
+
+  if (email !== undefined) {
+    preference.email = email;
+  }
+
+  if (sms !== undefined) {
+    preference.sms = sms;
+  }
+
+  if (push !== undefined) {
+    preference.push = push;
+  }
+
+  res.json(preference);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
